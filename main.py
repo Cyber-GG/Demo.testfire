757d98612b1ccc04e88bebc7b02a8b0e7e40a56d
Cyber-GG <160169067+Cyber-GG@users.noreply.github.com>
2024-02-16 03:57:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,12 @@ if element.is_displayed():
         time.sleep(8)
         current_url = driver.current_url
         print(current_url)
-  
+     
+    else: 
+     print("Some other page")
+     print(verify)
+     current_url = driver.current_url
+     print(current_url)
 else:
     print("Login page not loaded")
 
